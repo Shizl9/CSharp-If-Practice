0ad76ba518884cacd7cc9229963b0d354f994cb9
Sheika <sheika02x2@gmail.com>
2026-04-16 08:55:12 +0400

    add taske 2 odd or even

--- project 1/Program.cs
+++ project 1/Program.cs
@@ -1,10 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//*******Task1**********
 Console.WriteLine("please Enter numper:");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num > 0)
 { Console.WriteLine("positive"); }
 else if (num < 0)
 { Console.WriteLine("nigative"); }
-else if (num==0)
-    { Console.WriteLine("zero"); }
-else { Console.WriteLine("Invalid Input!");}
+else if (num == 0)
+{ Console.WriteLine("zero"); }
+else { Console.WriteLine("Invalid Input!"); }
+
+
+
+//*******Task2*********
+Console.WriteLine("Enter number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+{ Console.WriteLine("number is Even"); }
+else
+{ Console.WriteLine("number is Odd"); }
+
+//*******Task3*********
+
+
+
+
