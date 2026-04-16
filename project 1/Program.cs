bbb6b0a93006889d8a548d32f9df905e20638e92
Sheika <sheika02x2@gmail.com>
2026-04-16 08:22:41 +0400

    Add project files.

---
+++ project 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please Enter numper:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{ Console.WriteLine("positive"); }
+else if (num < 0)
+{ Console.WriteLine("nigative"); }
+else if (num==0)
+    { Console.WriteLine("zero"); }
+else { Console.WriteLine("Invalid Input!");}
